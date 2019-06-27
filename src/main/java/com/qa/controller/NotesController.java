403b6b3fe16425049d0b38c5dd56e107109712cd
tvaidotas <tadas.vaidotas@gmail.com>
2019-06-27 14:43:45 +0100

    added multiple endpoints

--- src/main/java/com/qa/controller/NotesController.java
+++ src/main/java/com/qa/controller/NotesController.java
@@ -1,16 +1,41 @@
 package com.qa.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.qa.model.Note;
+import com.qa.repository.NotesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
 public class NotesController {
 
+    @Autowired
+    private NotesRepository notesRepository;
+
     @RequestMapping(value = "notes", method = RequestMethod.GET)
     public List<Note> list(){
-
+        return notesRepository.findAll();
     }
+
+    @RequestMapping(value = "notes", method = RequestMethod.POST)
+    public Note create(@RequestBody Note note){
+        return notesRepository.saveAndFlush(note);
+    }
+
+    @RequestMapping(value = "notes/{id}", method = RequestMethod.GET)
+    public Note getNote(@PathVariable Long id){
+        return notesRepository.findOne(id);
+    }
+
+    @RequestMapping(value = "notes/{id}", method = RequestMethod.DELETE)
+    public Note delete(@PathVariable Long id){
+        Note existingNote = notesRepository.findOne(id);
+        notesRepository.delete(existingNote);
+        return existingNote;
+    }
+
+
 
 }
